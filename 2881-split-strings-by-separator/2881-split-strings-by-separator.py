301f5999bbbc3b9a90823b038f570c1e975e2a10
MohithIT203 <155316240+MohithIT203@users.noreply.github.com>
2025-02-20 14:53:02 +0530

    Time: 11 ms (36.84%), Space: 12.7 MB (21.58%) - LeetHub

--- 2881-split-strings-by-separator/2881-split-strings-by-separator.py
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.py
@@ -4,8 +4,6 @@ class Solution(object):
         temp=[]
         for i in range(len(words)):
             temp=words[i].split(separator)
-            for j in range(len(temp)):
-                if temp[j]!='':
-                    li.append(temp[j])
-        return li
+            li.extend(temp)
+        return filter(None,li)
         
