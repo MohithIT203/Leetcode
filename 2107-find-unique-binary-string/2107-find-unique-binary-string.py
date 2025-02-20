cbce03538cda1f9c21c6c6d26001806400978617
MohithIT203 <155316240+MohithIT203@users.noreply.github.com>
2025-02-20 22:55:29 +0530

    Time: 0 ms (100%), Space: 12.3 MB (97.09%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        n = len(nums)
+        return "".join('1' if nums[i][i] == '0' else '0' for i in range(n))
+
+
+        
