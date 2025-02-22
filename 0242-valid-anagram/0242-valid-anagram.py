e4b6169ee9a42641d6b4631b3b90122c20b694cc
MohithIT203 <155316240+MohithIT203@users.noreply.github.com>
2025-02-22 20:39:44 +0530

    Time: 23 ms (54.47%), Space: 12.7 MB (67.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,14 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        return sorted(s)==sorted(t)
+        # return sorted(s)==sorted(t)
+
+        if len(s)!=len(t):
+            return False
+        freq1=[0]*26
+        freq2=[0]*26
+        index=0
+        while(index<len(s)):
+            freq1[ord(s[index])-ord('a')]+=1
+            freq2[ord(t[index])-ord('a')]+=1
+            index+=1
+        return freq1==freq2
