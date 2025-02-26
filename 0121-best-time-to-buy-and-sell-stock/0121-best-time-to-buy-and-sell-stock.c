0fba7a79679474e803174ba578bb2ccc916a16e1
MohithIT203 <155316240+MohithIT203@users.noreply.github.com>
2025-02-26 21:54:40 +0530

    Time: 0 ms (100%), Space: 15.3 MB (42.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,4 +1,5 @@
 int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize < 2) return 0;
     int min = prices[0], profit = 0;
     for (int i = 1; i < pricesSize; i++) {
         if (prices[i] < min) {
