8f3a7db3669133e50dd527ae5584caa24c641ee1
MohithIT203 <155316240+MohithIT203@users.noreply.github.com>
2025-03-03 21:58:16 +0530

    Time: 74 ms (29.66%), Space: 28.3 MB (94.07%) - LeetHub

---
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def pivotArray(self, nums, pivot):
+        li1=[]
+        li2=[]
+        count=0
+        for num in nums:
+            if num<pivot:
+                li1.append(num)
+            elif num>pivot:
+                li2.append(num)
+            else:
+                count+=1
+        while(count!=0):
+            li1.append(pivot)
+            count-=1
+        li1.extend(li2)
+        return li1
