f1ef60fc207b0e0887876885d01d463c14c5b994
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-03-23 17:09:38 +0530

    Time: 4 ms (7.48%), Space: 12.4 MB (82.17%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def numberOfEmployeesWhoMetTarget(self, hours, target):
-        if max(hours)<target:
-            return 0
         count=0
         for num in hours:
             if num>=target:
