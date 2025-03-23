eecd0e9da56feaa7b428e697bb0c744847878459
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-03-23 16:28:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.26%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def sortTheStudents(self, score, k):
-        for i in range(len(score)):
-            for j in range(i,len(score)):
-                if(score[i][k]<score[j][k]):
-                    temp=score[i]
-                    score[i]=score[j]
-                    score[j]=temp
-                
-            
-        
+        # for i in range(len(score)):
+        #     for j in range(i,len(score)):
+        #         if(score[i][k]<score[j][k]):
+        #             temp=score[i]
+        #             score[i]=score[j]
+        #             score[j]=temp
+        score = sorted(score, key = lambda x:x[k], reverse=True)
         return score
+        
         
