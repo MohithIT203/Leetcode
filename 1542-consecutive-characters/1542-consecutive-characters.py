40a94f0d67c80c2de1658def57bb531b4664dc99
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-03-24 12:23:08 +0530

    Time: 15 ms (17.86%), Space: 12.5 MB (12.5%) - LeetHub

--- 1542-consecutive-characters/1542-consecutive-characters.py
+++ 1542-consecutive-characters/1542-consecutive-characters.py
@@ -5,9 +5,11 @@ class Solution(object):
         maxi=count
         while(end<len(s)):
             if s[start]==s[end]:
+                count+=1
                 end+=1
             else:
                 start=end
-            count=end-start
-            maxi=max(count,maxi)
+                maxi=max(count,maxi)
+                count=0
+        maxi=max(count,maxi)
         return maxi
