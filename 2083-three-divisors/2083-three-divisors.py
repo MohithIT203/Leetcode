703b3f39e7742a3d601c607dbb7de9c1669ff57f
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-03-25 21:04:03 +0530

    Time: 7 ms (33.55%), Space: 12.7 MB (39.65%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.py
+++ 2083-three-divisors/2083-three-divisors.py
@@ -6,8 +6,8 @@ class Solution(object):
         for i in range(1,n+1):
             if n%i==0:
                 count+=1
-        if count<3 or count>3:
-            return False
-        return True
+        if count==3:
+            return True
+        return False
         
         
