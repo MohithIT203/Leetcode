051f964a24efb61298b71cb8e313d28f923c9d54
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-03-25 20:55:21 +0530

    Time: 0 ms (100%), Space: 12.5 MB (55.58%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
@@ -3,7 +3,6 @@ class Solution(object):
         maximum=max(nums)
         minimum=min(nums)
         for i in range(minimum,0,-1):
-            print(i)
             if maximum%i==0 and minimum%i==0:
                 return i
         return 1
