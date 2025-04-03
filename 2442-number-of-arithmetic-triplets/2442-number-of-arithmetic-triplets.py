7d9a30183163f3b1854d932bc4c8ad35d0a8aa38
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-04-03 10:14:35 +0530

    Time: 0 ms (100%), Space: 12.6 MB (14.1%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
@@ -1,9 +1,20 @@
 class Solution(object):
     def arithmeticTriplets(self, nums, diff):
+        # count=0
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         for k in range(j+1,len(nums)):
+        #             if nums[j]-nums[i]==diff and nums[k]-nums[j]==diff:
+        #                 count+=1
+        # return count
+
+#         b=a+diff
+# \U0001d450=\U0001d44f+diff=\U0001d44e+2×diff
+# c=b+diff=a+2×diff
         count=0
+        unique=set(nums)
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    if nums[j]-nums[i]==diff and nums[k]-nums[j]==diff:
-                        count+=1
+            if nums[i]+diff in unique:
+                if nums[i]+2*diff in unique:
+                    count+=1
         return count
