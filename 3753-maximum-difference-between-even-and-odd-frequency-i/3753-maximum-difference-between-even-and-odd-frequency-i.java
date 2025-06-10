f5233604ddb56abf9880f39bc05c39540d4cdb3a
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-06-10 23:09:21 +0530

    Time: 1 ms (100%), Space: 42.3 MB (89.02%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
@@ -1,10 +1,8 @@
 class Solution {
     public int maxDifference(String s) {
-        Scanner in=new Scanner(System.in);
         int [] freq=new int [26];
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            freq[c-'a']++;
+            freq[(s.charAt(i))-'a']++;
         }
         int maxi=0,mini=s.length();
         for(int i=0;i<26;i++){
