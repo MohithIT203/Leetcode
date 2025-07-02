668be98a12e381175316dc680d424f18989123bb
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-07-02 13:50:46 +0530

    Time: 7 ms (94.91%), Space: 60.1 MB (99.69%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -10,15 +10,36 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        Set<Integer> arr = new HashSet<>();
-        for (int n : nums)
-            arr.add(n);
-        while (head != null && arr.contains(head.val)) {
+        // Set<Integer> arr = new HashSet<>();
+        // for (int n : nums)
+        //     arr.add(n);
+        // while (head != null && arr.contains(head.val)) {
+        //     head = head.next;
+        // }
+        // ListNode ptr = head;
+        // while (ptr != null && ptr.next != null) {
+        //     if (arr.contains(ptr.next.val)) {
+        //         ptr.next = ptr.next.next;
+        //     } else {
+        //         ptr = ptr.next;
+        //     }
+        // }
+        // return head;
+
+
+        int max=-1;
+        for(int n:nums){
+            if(max<n) max=n;
+        }
+        boolean arr[]=new boolean[max+1];
+        Arrays.fill(arr,false);
+        for(int n:nums) arr[n]=true;
+        while (head != null && head.val<=max && arr[head.val]==true) {
             head = head.next;
         }
-        ListNode ptr = head;
-        while (ptr != null && ptr.next != null) {
-            if (arr.contains(ptr.next.val)) {
+        ListNode ptr=head;
+         while (ptr != null && ptr.next != null) {
+            if ((ptr.next.val<=max) && (arr[ptr.next.val]==true)) {
                 ptr.next = ptr.next.next;
             } else {
                 ptr = ptr.next;
