d48c2dac789c45735693343c1ae31b233ea69d05
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-07-02 11:45:56 +0530

    Time: 25 ms (13.39%), Space: 66.7 MB (26.39%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -10,18 +10,18 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        Set<Integer> arr=new HashSet<>();
-        for(int n:nums) arr.add(n);
-        while (head != null && arr.contains(head.val)) { //Remove beginning vals
+        Set<Integer> arr = new HashSet<>();
+        for (int n : nums)
+            arr.add(n);
+        while (head != null && arr.contains(head.val)) {
             head = head.next;
         }
-        ListNode ptr=head; 
-        while(ptr!=null && ptr.next!=null){
-            if(arr.contains(ptr.next.val)){
-                ptr.next=ptr.next.next;
-            }
-            else{
-            ptr=ptr.next;
+        ListNode ptr = head;
+        while (ptr != null && ptr.next != null) {
+            if (arr.contains(ptr.next.val)) {
+                ptr.next = ptr.next.next;
+            } else {
+                ptr = ptr.next;
             }
         }
         return head;
