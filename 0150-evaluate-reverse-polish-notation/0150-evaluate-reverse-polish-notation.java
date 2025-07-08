0e9dbcef5248746c2bc875680f1a98d99442c83e
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-07-08 15:58:02 +0530

    Time: 32 ms (5.22%), Space: 47.1 MB (6.94%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -6,25 +6,15 @@ class Solution {
                 stack.push(Integer.parseInt(token));
             }
             else if(stack.size()>=2 && token.matches("[+\\-*/^]")){
-
                 int n2=stack.pop();
                 int n1=stack.pop();
-                stack.push(Eval(n1,n2,token));
+                if(token.equals("+")) stack.push(n1+n2);
+                else if(token.equals("-"))stack.push(n1-n2);
+                else if(token.equals("*"))stack.push(n1*n2);
+                else stack.push((int)n1/n2);
             }
         }
         return stack.peek();
     } 
-    public int Eval(int n1,int n2,String token){
-        switch(token){
-            case "+":
-            return n1+n2;
-            case "-":
-            return n1-n2;
-            case "*":
-            return n1*n2;
-            case "/":
-            return (int)n1/n2;
-        }
-        return 0;
-    }
+    
 }
