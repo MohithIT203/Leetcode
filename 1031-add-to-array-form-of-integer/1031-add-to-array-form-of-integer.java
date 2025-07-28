509908e5e4c539db2d19d90977bd9d8398afd9a7
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-07-28 23:01:13 +0530

    Time: 3 ms (58.37%), Space: 45.9 MB (61.27%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
@@ -1,18 +1,15 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
         List<Integer> list=new ArrayList<>();
-        Stack<Integer> stack=new Stack<>();
         int i=num.length-1,carry=0;
         while(i>=0||carry!=0||k!=0){
             int n=(i>=0)?num[i--]:0;
             int ans=n+(k%10)+carry;
             k/=10;
-            stack.add(ans%10);
+            list.add(ans%10);
             carry=ans/10;
         }
-        while(!stack.isEmpty()){
-            list.add(stack.pop());
-        }
+        Collections.reverse(list);
         return list;
     }
 }
