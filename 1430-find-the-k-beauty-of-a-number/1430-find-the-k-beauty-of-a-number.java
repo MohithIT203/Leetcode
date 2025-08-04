5cbe9d96ecccb585d36c512b806ef08a288389fb
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-08-04 22:26:36 +0530

    Time: 0 ms (100%), Space: 40.5 MB (58.6%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
@@ -1,26 +1,18 @@
 class Solution {
-    public int divisorSubstrings(int num, int k) {
+    public int divisorSubstrings(int num, int k) 
+    {
+        String str = String.valueOf(num);
         int count=0;
-        String n=Integer.toString(num);
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<k;i++){
-            sb.append(Character.toString(n.charAt(i)));
-        }
-        int i=k;
-        int divisor=0;
-        while(i<n.length()){
-            divisor=Integer.parseInt(sb.toString());
-            if(divisor!=0 && num%divisor==0){
+        int n=str.length();
+        for(int i=0;i<=n-k;i++)
+        {
+            String sub = str.substring(i , i+k);
+            int divisor = Integer.parseInt(sub);
+
+            if (divisor != 0 && num % divisor == 0) {
                 count++;
             }
-            sb.deleteCharAt(0);
-            sb.append(Character.toString(n.charAt(i)));
-            i++;
         }
-            divisor=Integer.parseInt(sb.toString());
-            if(divisor!=0 && num%divisor==0){
-                count++;
-            }
         return count;
     }
 }
