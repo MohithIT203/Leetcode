1d420d0405598aca6689e74ff448d317a141433e
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2025-12-12 22:43:51 +0530

    Time: 20 ms (76.11%), Space: 53.8 MB (45.81%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -35,35 +35,35 @@ class Solution {
 
     int lcs_wrapper(String str1, String str2) { //wrapper
         // lcs_rec(text1,0,text2,0);//recursion
-        int[][] memo = new int[str1.length()][str2.length()];
-        for (int row = 0; row < str1.length(); row++) {
-            Arrays.fill(memo[row], -1);
-        }
-        return lcs_memo(str1, 0, str2, 0, memo);
+        // int[][] memo = new int[str1.length()][str2.length()];
+        // for (int row = 0; row < str1.length(); row++) {
+        //     Arrays.fill(memo[row], -1);
+        // }
+        // return lcs_memo(str1, 0, str2, 0, memo);
 
         //dp-tabulation
-        // int[][] tab = new int[str1.length() + 1][str2.length() + 1];
-        // int row, col;
-        // for (row = str1.length(), col = 0; col <= str2.length(); col++)
-        //     tab[row][col] = 0;
-        // for (col = str2.length(), row = 0; row <= str1.length(); row++)
-        //     tab[row][col] = 0;
-        // int ind1;
-        // int ind2;
-        // for (ind1 = str1.length() - 1; ind1 >= 0; ind1--) {
-        //     for (ind2 = str2.length() - 1; ind2 >= 0; ind2--) {
-        //         int lcs_len = 0;
-        //         if (str1.charAt(ind1) == str2.charAt(ind2))//equal
-        //             lcs_len = 1 + tab[ind1 + 1][ind2 + 1];
-        //         else {
-        //             int ch1 = tab[ind1 + 1][ind2];
-        //             int ch2 = tab[ind1][ind2 + 1];
-        //             lcs_len = Math.max(ch1, ch2);
-        //         }
-        //         tab[ind1][ind2] = lcs_len;
-        //     }
-        // }
-        // return tab[0][0];
+        int[][] tab = new int[str1.length() + 1][str2.length() + 1];
+        int row, col;
+        for (row = str1.length(), col = 0; col <= str2.length(); col++)
+            tab[row][col] = 0;
+        for (col = str2.length(), row = 0; row <= str1.length(); row++)
+            tab[row][col] = 0;
+        int ind1;
+        int ind2;
+        for (ind1 = str1.length() - 1; ind1 >= 0; ind1--) {
+            for (ind2 = str2.length() - 1; ind2 >= 0; ind2--) {
+                int lcs_len = 0;
+                if (str1.charAt(ind1) == str2.charAt(ind2))//equal
+                    lcs_len = 1 + tab[ind1 + 1][ind2 + 1];
+                else {
+                    int ch1 = tab[ind1 + 1][ind2];
+                    int ch2 = tab[ind1][ind2 + 1];
+                    lcs_len = Math.max(ch1, ch2);
+                }
+                tab[ind1][ind2] = lcs_len;
+            }
+        }
+        return tab[0][0];
 
     }
 
