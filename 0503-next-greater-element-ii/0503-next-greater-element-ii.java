7c0f20404413038be5961e3c9f7bda941a1c5b07
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2026-01-28 11:38:51 +0530

    Time: 14 ms (62.27%), Space: 48.6 MB (40.25%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -2,17 +2,22 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n=nums.length;
         int fin[] = new int[n];
-        Arrays.fill(fin,-1);
         Stack<Integer> stack = new Stack<>(); //index
         //duplicate exists --> no value based mapping
-        for(int i=0;i<2*n;i++){
-            int index=i%n;
-            while(!stack.isEmpty() && nums[stack.peek()]<nums[index]){
-                fin[stack.pop()]=nums[index];
+        for(int i=2*n-1;i>=0;i--){
+            int curr=nums[i%n];
+            while(!stack.isEmpty() && stack.peek()<=curr){
+                stack.pop();
             }
             if(i<n){
-                stack.push(index);
+                if(stack.isEmpty()){
+                    fin[i]=-1;
+                }
+                else{
+                    fin[i]=stack.peek();
+                }   
             }
+            stack.push(curr);
         }
         return fin;
     }
