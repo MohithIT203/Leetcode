209e152377a8c5f6325c65dac2755f936d00744e
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2026-02-04 23:32:50 +0530

    Time: 1 ms (87.98%), Space: 44.2 MB (53.02%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,35 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> set=new ArrayList<>();
-        List<Integer> res=new ArrayList<>();
-        recursion(0,nums,set,res);
-        return set;
-    }
-    public void recursion(int i,int[] nums,List<List<Integer>> set, List<Integer> res){
-        if(i==nums.length){
-            set.add(new ArrayList<>(res));
-            return;
+    void fun_v1(int [] arr,int wind,List<Integer> seq,List<List<Integer>> res)
+    {
+        if(wind==arr.length) 
+        {
+            res.add(new ArrayList<>(seq));//System.out.println(seq);
+         return;
         }
-        res.add(nums[i]);
-        recursion(i+1,nums,set,res);
-        res.remove(res.size()-1);
-        recursion(i+1,nums,set,res);
+        fun_v1(arr,wind+1,seq,res);//exclude and work for next
+        seq.add(arr[wind]);//do
+        fun_v1(arr,wind+1,seq,res);//include and work for next
+        seq.remove(seq.size()-1);//undo
+    }
+    void fun_v2(int [] arr,int wind,List<Integer> seq,List<List<Integer>> res)
+    {
+        res.add(new ArrayList<Integer>(seq));
+        if(wind==arr.length) return;
+        for(int ind=wind;ind<arr.length;ind++)//list choices
+        {
+            seq.add(arr[ind]);//do
+            fun_v2(arr,ind+1,seq,res);
+            seq.remove(seq.size()-1);//undo
+        }
+     }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res= new ArrayList<List<Integer>>();
+        List<Integer> seq=new ArrayList<>();
+        //fun_v1(nums,0,seq,res);
+        fun_v2(nums,0,seq,res);
+        
+        return res;
+
     }
 }
