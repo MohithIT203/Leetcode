4c8332b02078a676eb9ec27aa63a8a13371f6d02
Mohith M <155316240+MohithIT203@users.noreply.github.com>
2026-03-11 07:59:47 +0530

    Time: 4 ms (3.22%), Space: 42.8 MB (7.96%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findComplement(int n) {
+        if(n == 0) return 1;
+        String bin="";
+        int comp = 0;
+        while(n!=0){
+            bin=(n%2)+bin;
+            n/=2;
+        }
+        int temp =0;
+        for(int i=bin.length()-1;i>=0;i--){
+            if(bin.charAt(i)=='0'){
+                comp+=(1<<temp);
+            }
+            temp++;
+        }
+        return comp;
+    }
+}
